2e3c795fd17d123d7cc6beba8c56747451877ead
AkashSingh2218271 <akrawat0056@gmail.com>
2025-08-09 20:49:47 +0530

    Adding code for Igniting our app from scratch

---
+++ Igniting/app.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+import ReactDOM from 'react-dom/client'
+
+const h1 = React.createElement("h1",{
+    id : "headingOne",
+}, "Heading One");
+
+const h2 = React.createElement("h2",{
+    id : "headingTwo",
+}, "Heading Two");
+
+const container = React.createElement("div", {
+    id : "container"
+}, [h1, h2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
